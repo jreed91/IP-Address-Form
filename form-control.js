262807f574aa160d44146bef59c65cd76e2be0ac
jreed91 <jreed91@gmail.com>
2015-09-10 10:20:02 -0500

    rewrite to jquery plugin

--- form-control.js
+++ form-control.js
@@ -1,76 +1,185 @@
-function isNumber(evt) {
-    var returnVal = true,
-        charCode = (evt.which) ? evt.which : evt.keyCode;
-    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
-        if ((evt.ctrlKey || evt.metaKey) && (charCode == 97 || charCode == 99 || charCode == 118)) {
-            returnVal = true;
-        }
-        returnVal = false;
-    }
-    return returnVal;
-}
-var moveToNext = function (evt, element, prevElement, nextElement, numberOfDigitsToValidate) {
-    var charCode = (evt.which) ? evt.which : evt.keyCode;
-    if (charCode == 13) {
-        validateAndSubmit(numberOfDigitsToValidate);
-    } else {
-        if (element.value != null && element.value != '' && element.value != 'undefined' && element.value.length > 2 || charCode == 190 || charCode == 110) {
-            if (nextElement) {
-                document.getElementById(nextElement).focus();
-            }
-        }
-        if (charCode == 37) {
-            document.getElementById(prevElement).focus();
+(function($) {
+    $.widget('ui.ipComplete', {
+        options: {
+           delimiter: '.',
+           octetLimit          : 4,   // Max number of tags allowed (null for unlimited).
+            beforeOctetAdded      : null,
+            afterOctetAdded       : null,
 
-        }
-        if (charCode == 39) {
-            document.getElementById(nextElement).focus();
-        }
-    }
+            beforeOctetRemoved    : null,
+            afterOctetRemoved     : null,
 
-}
-var disableBackSpace = function (evt, element, prevElement) {
-    var charCode = (evt.which) ? evt.which : evt.keyCode;
-    if (charCode === 8 || charCode === 46) {
-        if (element.value != null && element.value != '' && element.value != 'undefined') {
-            element.value = '';
-        } else {
-            if (prevElement != null) {
-                document.getElementById(prevElement).value = "";
-                document.getElementById(prevElement).focus();
-            }
-        }
-    }
-}
+            onOctetClicked        : null,
+            onOctetLimitExceeded  : null,
+            allowIPv6             : false
+        },
 
-var pasteValues = function (element) {
+        _create: function() {
+            var that = this;
 
-    setTimeout(function () {
-        var values = element.value.split(".");
-        var regex = /^\d+$/;
-
-
-        for (var index in values) {
-            if (values[index].length < 4 && regex.test(values[index])) {
-                var location = index;
-                var inputBox = 'ip' + (location++) + '';
-                document.getElementById(inputBox).value = values[index];
-            }
-            else {
-                var nonint = values[index].replace(/\D/g, '');
-                var location = index;
-                var inputBox = 'ip' + (location++) + '';
-                document.getElementById(inputBox).value = nonint.substring(0, 3);
+            if (this.element.is('input')) {
+                this.ipList = $('<ul></ul>').insertAfter(this.element);
+                this.element.addClass('ip-hidden-field');
+            } else {
+                this.ipList = this.element.find('ul, ol').andSelf().last();
             }
 
-        };
-            if (document.getElementById('ip3').value != '') {
-                document.getElementById('ip3').focus();
+            this.ipInput = $('<input type="text" />').addClass('ui-widget-content')
+
+            this.ipList
+                .addClass('ip')
+                .addClass('ui-widget ui-widget-content ui-corner-all')
+
+                .append($('<li class="ip-new"></li>').append(this.ipInput))
+                .click(function(e) {
+                    var target = $(e.target);
+                    if (target.hasClass('ip-octet')) {
+                        var octet = target.closest('.ip-choice');
+                        if(!octet.hasClass('remove')) {
+                            that._trigger('onOctetClicked', e, {octet: octet, octetLabel: that.octetLabel(octet)});
+                        } else {
+                            that.ipInput.focus();
+                        }
+                    }
+                });
+            this.ipInput
+                .keydown(function(event) {
+                    if (event.which == $.ui.keyCode.BACKSPACE && that.ipInput.val() === '') {
+                        var octet = that._lastOctet();
+                        // When backspace is pressed, the last octet is deleted.
+                        that.removeOctet(octet);
+                    }
+                    if (that.ipInput.val().length > 2) {
+                        that.createOctet(that._cleanedInput());
+                    }
+                    switch( event.keyCode ) {
+                        case $.ui.keyCode.PERIOD:
+                          console.log('period')
+                          that.createOctet(that._cleanedInput());
+                          break;
+                        case 110:
+                          that.createOctet(that._cleanedInput());
+                          break;
+                      }
+                    });
+        },
+
+       
+
+        destroy: function() {
+            $.Widget.prototype.destroy.call(this);
+
+            this.element.unbind('.ip');
+            this.ipList.unbind('.ip');
+
+            this.ipList.removeClass([
+                'ip',
+                'ui-widget',
+                'ui-widget-content',
+                'ui-corner-all',
+                'ip-hidden-field'
+                ].join(' '));
+
+            if (this.element.is('input')) {
+                this.element.removeClass('ip-hidden-field');
+                this.ipList.remove();
+            } else {
+                this.element.children('li').each(function() {
+                    if ($(this).hasClass('ip-new')) {
+                        $(this).remove();
+                    } else {
+                        $(this).removeClass([
+                            'ip-choice',
+                            'ui-widget-content',
+                            'ui-state-default',
+                            'ui-state-highlight',
+                            'ui-corner-all',
+                            'remove'
+                        ].join(' '));
+                    }
+                });
+
+                if (this.singleFieldNode) {
+                    this.singleFieldNode.remove();
+                }
             }
-            else
-            {
+
+            return this;
+        },
+
+         _cleanedInput: function() {
+            // Returns the contents of the tag input, cleaned and ready to be passed to createTag
+            return $.trim(this.ipInput.val().replace('.', ''));
+        },
+
+         _octets: function() {
+            return this.ipList.find('.ip-choice:not(.removed)');
+        },
+
+        _lastOctet: function() {
+            return this.ipList.find('.ip-choice:last:not(.removed)');
+        },
+
+        createOctet: function(value, additionalClass, duringInitialization) {
+            var that = this;
+
+            value = $.trim(value);
+
+            if (value === '') {
+                return false;
+            }
+
+            if (this.options.octetLimit && this._octets().length >= this.options.octetLimit) {
+                this._trigger('onOctetLimitExceeded', null, {duringInitialization: duringInitialization});
+                return false;
+            }
+
+            var label = $(this.options.onOctetClicked ? '<a class="ip-label"</a>' : '<span class="ip-label"></span>').text(value);
+
+            var octet = $('<li></li>')
+                .addClass('ip-choice ui-widget-content ui-state-default ui-corner-all')
+                .append(label);
+            var decimal = $('<li>.</li>');
+
+            octet.addClass('ip-choice-editable');
+
+
+            if (this._trigger('beforeOctetAdded', null, {
+                octet: octet,
+                octetLabel: this.octetLabel(octet),
+                duringInitialization: duringInitialization
+            }) === false) {
                 return;
             }
-    }, 0); //or 4
-};
+
+            this.ipInput.parent().before(octet);
+            this.ipInput.val('');
+
+            this._trigger('afterOctetAdded', null, {
+                octet: octet,
+                octetLabel: this.octetLabel(octet),
+                duringInitialization: duringInitialization
+            });
+
+        },
+
+        removeOctet: function(octet) {
+            octet = $(octet);
+             this._trigger('onOctetRemoved', null, octet);
+
+            if (this._trigger('beforeOctetRemoved', null, {octet: octet, octetLabel: this.octetLabel(octet)}) === false) {
+                return;
+            }
+
+            octet.remove();
+            this._trigger('afterOctetRemoved', null, {octet: octet, octetLabel: this.octetLabel(octet)});
+        },
+
+        octetLabel: function(octet) {
+            // Returns the tag's string label.
+            return $(octet).find('input:first').val();
+        },
+
+    })
+}(jQuery));
 
